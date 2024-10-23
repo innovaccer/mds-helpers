6e29456d6adb830026cead5e5198045fb84e1297
Anuradha Aggarwal <anuradha.mcs18.du@gmail.com>
2024-10-23 12:24:09 +0400

    chore: migrate to node 20 and fix security vulnerability (#13)

--- src/utils/__tests__/alertServiceHelper.test.tsx
+++ src/utils/__tests__/alertServiceHelper.test.tsx
@@ -1,7 +1,8 @@
 import { pubSub } from '../alertServiceHelper';
 describe('pubSub', () => {
   it('should provide be UTC', () => {
-    expect(JSON.stringify(pubSub()))
-    .toEqual(JSON.stringify({ publish: function publish() {}, subscribe: function subscribe() {} }));
+    expect(JSON.stringify(pubSub())).toEqual(
+      JSON.stringify({ publish: function publish() {}, subscribe: function subscribe() {} })
+    );
   });
 });
